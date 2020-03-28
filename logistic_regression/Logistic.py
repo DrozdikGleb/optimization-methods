6af09f2812868381e5ce546fb2e802e4c8792cc1
Vitaliy Glamozda <v_glamozda@wargaming.net>
2020-03-28 15:06:55 +0300

    Added evaluation in matrix form

--- logistic_regression/Logistic.py
+++ logistic_regression/Logistic.py
@@ -5,7 +5,6 @@ from scipy.special import expit
 
 from Utils import k_fold_cross_validation, f_measure
 
-
 CLASSES_NUMBER = 2
 
 
@@ -28,22 +27,31 @@ class Logistic:
 		self.w = np.random.normal(loc=0., scale=1., size=m)
 
 	def __evaluate_Q(self, X, y, n):
-		losses = np.sum([np.logaddexp(0, -y[i] * np.dot(X[i], self.w)) for i in range(n)])
-		regularization = self.lmb / 2 * np.sum(self.w ** 2)
-		return (losses / n) + regularization
+		predictions = np.matmul(X, self.w)
+		margins = predictions * y
+		losses = np.logaddexp(0, -margins)
+		return 1 / n * losses + self.lmb / 2 * np.sum(np.square(self.w))
+
+	def __evaluate_gradient_Q(self, X, y, n):
+		predictions = np.matmul(X, self.w)
+		margins = predictions * y
+		b = expit(-margins)
+		A = np.transpose(X * y.reshape((n, 1)))
+		return -1 / n * np.matmul(A, b) + self.lmb * self.w
+
+	def __evaluate_hessian_Q(self, X, y, n):
+		predictions = np.matmul(X, self.w)
+		margins = predictions * y
+		C = np.transpose(X * expit(-margins).reshape((n, 1)))
+		D = X * expit(margins).reshape((n, 1))
+		return 1 / n * np.matmul(C, D) + self.lmb * np.eye(m)
 
 	def fit(self, X, y):
 		n, m = X.shape
 		self.__init_weight(m)
 		Q = self.__evaluate_Q(X, y, n)
 		for s in range(self.amount_steps):
-			gradient = np.ndarray(m)
-			for j in range(m):
-				for i in range(n):
-					gradient[j] += expit(-y[i] * np.dot(X[i], self.w)) * y[i] * X[i][j]
-				gradient[j] *= -1 / n
-				gradient[j] += self.lmb * self.w[j]
-			self.w = self.w - self.learning_rate * gradient
+			self.w = self.w - self.learning_rate * self.__evaluate_gradient_Q(X, y, n)
 
 			new_Q = self.__evaluate_Q(X, y, n)
 			if np.linalg.norm(new_Q - Q) < self.eps:
@@ -94,11 +102,12 @@ def make_plot(X, y, lmb, learning_rate):
 
 if __name__ == '__main__':
 	X, y = read_dataset('data/geyser.csv')
+	n, m = X.shape
 	best_F = -1
 	best_lmb = None
 	best_rate = None
-	for lmb in [0.0001, 0.001, 0.01, 0.1, 1.]:
-		for learning_rate in [1e-3, 1e-8]:
+	for lmb in [1e-4, 1e-3, 1e-2, 1e-1, 1.]:
+		for learning_rate in [1e-3, 1e-5, 1e-8]:
 			f_score = train_dataset(X, y, lmb, learning_rate)
 			if f_score > best_F:
 				best_F = f_score
